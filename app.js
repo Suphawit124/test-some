f60476b2bb50a116ccaa7259d83b480eeb1ffe2a
Suphawit Watthanataetakun <61160124@go.buu.ac.th>
2021-10-10 21:24:41 +0700

    test5

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const port = process.env.port || '0.0.0.0'
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 const app = express()
 
 app.get('/', (req, res) => {
     res.json({message: 'Hello world!'})
 })
 
-app.listen(port, () => {
+app.listen(port, host, () => {
     console.log('The app is up and running!')
 })
 
